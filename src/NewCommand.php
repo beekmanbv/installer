34b893abc3923548e4afdb78c02f306becbe3fa6
Mark Eilander <marke@beekman.nl>
2023-09-13 13:23:32 +0200

    no message

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -39,26 +39,8 @@ class NewCommand extends Command
     {
         $this
             ->setName('new')
-            ->setDescription('Create a new Laravel application')
+            ->setDescription('Create a new Beekman Laravel application')
             ->addArgument('name', InputArgument::REQUIRED)
-            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
-            ->addOption('git', null, InputOption::VALUE_NONE, 'Initialize a Git repository')
-            ->addOption('branch', null, InputOption::VALUE_REQUIRED, 'The branch that should be created for a new repository', $this->defaultBranch())
-            ->addOption('github', null, InputOption::VALUE_OPTIONAL, 'Create a new repository on GitHub', false)
-            ->addOption('organization', null, InputOption::VALUE_REQUIRED, 'The GitHub organization to create the new repository for')
-            ->addOption('stack', null, InputOption::VALUE_OPTIONAL, 'The Breeze / Jetstream stack that should be installed')
-            ->addOption('breeze', null, InputOption::VALUE_NONE, 'Installs the Laravel Breeze scaffolding')
-            ->addOption('jet', null, InputOption::VALUE_NONE, 'Installs the Laravel Jetstream scaffolding')
-            ->addOption('dark', null, InputOption::VALUE_NONE, 'Indicate whether Breeze or Jetstream should be scaffolded with dark mode support')
-            ->addOption('typescript', null, InputOption::VALUE_NONE, 'Indicate whether Breeze should be scaffolded with TypeScript support (Experimental)')
-            ->addOption('ssr', null, InputOption::VALUE_NONE, 'Indicate whether Breeze or Jetstream should be scaffolded with Inertia SSR support')
-            ->addOption('api', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with API support')
-            ->addOption('teams', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with team support')
-            ->addOption('verification', null, InputOption::VALUE_NONE, 'Indicates whether Jetstream should be scaffolded with email verification support')
-            ->addOption('pest', null, InputOption::VALUE_NONE, 'Installs the Pest testing framework')
-            ->addOption('phpunit', null, InputOption::VALUE_NONE, 'Installs the PHPUnit testing framework')
-            ->addOption('prompt-breeze', null, InputOption::VALUE_NONE, 'Issues a prompt to determine if Breeze should be installed (Deprecated)')
-            ->addOption('prompt-jetstream', null, InputOption::VALUE_NONE, 'Issues a prompt to determine if Jetstream should be installed (Deprecated)')
             ->addOption('force', 'f', InputOption::VALUE_NONE, 'Forces install even if the directory already exists');
     }
 
@@ -75,12 +57,7 @@ class NewCommand extends Command
 
         $this->configurePrompts($input, $output);
 
-        $output->write(PHP_EOL.'  <fg=red> _                               _
-  | |                             | |
-  | |     __ _ _ __ __ ___   _____| |
-  | |    / _` | \'__/ _` \ \ / / _ \ |
-  | |___| (_| | | | (_| |\ V /  __/ |
-  |______\__,_|_|  \__,_| \_/ \___|_|</>'.PHP_EOL.PHP_EOL);
+        $output->write(PHP_EOL.'Beekman Laravel installer'.PHP_EOL.PHP_EOL);
 
         if (! $input->getArgument('name')) {
             $input->setArgument('name', text(
@@ -91,38 +68,6 @@ class NewCommand extends Command
                     ? 'The name may only contain letters, numbers, dashes, underscores, and periods.'
                     : null,
             ));
-        }
-
-        if (! $input->getOption('breeze') && ! $input->getOption('jet')) {
-            match (select(
-                label: 'Would you like to install a starter kit?',
-                options: [
-                    'none' => 'No starter kit',
-                    'breeze' => 'Laravel Breeze',
-                    'jetstream' => 'Laravel Jetstream',
-                ],
-            )) {
-                'breeze' => $input->setOption('breeze', true),
-                'jetstream' => $input->setOption('jet', true),
-                default => null,
-            };
-        }
-
-        if ($input->getOption('breeze')) {
-            $this->promptForBreezeOptions($input);
-        } elseif ($input->getOption('jet')) {
-            $this->promptForJetstreamOptions($input);
-        }
-
-        if (! $input->getOption('phpunit') && ! $input->getOption('pest')) {
-            $input->setOption('pest', select(
-                label: 'Which testing framework do you prefer?',
-                options: ['PHPUnit', 'Pest'],
-            ) === 'Pest');
-        }
-
-        if (! $input->getOption('git') && $input->getOption('github') === false && Process::fromShellCommandline('git --version')->run() === 0) {
-            $input->setOption('git', confirm(label: 'Would you like to initialize a Git repository?', default: false));
         }
     }
 
@@ -135,8 +80,6 @@ class NewCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateStackOption($input);
-
         $name = $input->getArgument('name');
 
         $directory = $name !== '.' ? getcwd().'/'.$name : '.';
@@ -184,43 +127,10 @@ class NewCommand extends Command
                 $this->configureDefaultDatabaseConnection($directory, $database, $name);
             }
 
-            if ($input->getOption('git') || $input->getOption('github') !== false) {
-                $this->createRepository($directory, $input, $output);
-            }
-
-            if ($input->getOption('breeze')) {
-                $this->installBreeze($directory, $input, $output);
-            } elseif ($input->getOption('jet')) {
-                $this->installJetstream($directory, $input, $output);
-            } elseif ($input->getOption('pest')) {
-                $this->installPest($directory, $input, $output);
-            }
-
-            if ($input->getOption('github') !== false) {
-                $this->pushToGitHub($name, $directory, $input, $output);
-                $output->writeln('');
-            }
-
             $output->writeln("  <bg=blue;fg=white> INFO </> Application ready in <options=bold>[{$name}]</>. Build something amazing.".PHP_EOL);
         }
 
         return $process->getExitCode();
-    }
-
-    /**
-     * Return the local machine's default Git branch if set or default to `main`.
-     *
-     * @return string
-     */
-    protected function defaultBranch()
-    {
-        $process = new Process(['git', 'config', '--global', 'init.defaultBranch']);
-
-        $process->run();
-
-        $output = trim($process->getOutput());
-
-        return $process->isSuccessful() && $output ? $output : 'main';
     }
 
     /**
@@ -299,62 +209,6 @@ class NewCommand extends Command
     }
 
     /**
-     * Install Laravel Breeze into the application.
-     *
-     * @param  string  $directory
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function installBreeze(string $directory, InputInterface $input, OutputInterface $output)
-    {
-        $commands = array_filter([
-            $this->findComposer().' require laravel/breeze',
-            trim(sprintf(
-                $this->phpBinary().' artisan breeze:install %s %s %s %s %s',
-                $input->getOption('stack'),
-                $input->getOption('typescript') ? '--typescript' : '',
-                $input->getOption('pest') ? '--pest' : '',
-                $input->getOption('dark') ? '--dark' : '',
-                $input->getOption('ssr') ? '--ssr' : '',
-            )),
-        ]);
-
-        $this->runCommands($commands, $input, $output, workingPath: $directory);
-
-        $this->commitChanges('Install Breeze', $directory, $input, $output);
-    }
-
-    /**
-     * Install Laravel Jetstream into the application.
-     *
-     * @param  string  $directory
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function installJetstream(string $directory, InputInterface $input, OutputInterface $output)
-    {
-        $commands = array_filter([
-            $this->findComposer().' require laravel/jetstream',
-            trim(sprintf(
-                $this->phpBinary().' artisan jetstream:install %s %s %s %s %s %s %s',
-                $input->getOption('stack'),
-                $input->getOption('api') ? '--api' : '',
-                $input->getOption('dark') ? '--dark' : '',
-                $input->getOption('teams') ? '--teams' : '',
-                $input->getOption('pest') ? '--pest' : '',
-                $input->getOption('verification') ? '--verification' : '',
-                $input->getOption('ssr') ? '--ssr' : '',
-            )),
-        ]);
-
-        $this->runCommands($commands, $input, $output, workingPath: $directory);
-
-        $this->commitChanges('Install Jetstream', $directory, $input, $output);
-    }
-
-    /**
      * Determine the default database connection.
      *
      * @param  \Symfony\Component\Console\Input\InputInterface  $input
@@ -378,214 +232,6 @@ class NewCommand extends Command
         }
 
         return $database;
-    }
-
-    /**
-     * Determine the stack for Breeze.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @return void
-     */
-    protected function promptForBreezeOptions(InputInterface $input)
-    {
-        if (! $input->getOption('stack')) {
-            $input->setOption('stack', select(
-                label: 'Which Breeze stack would you like to install?',
-                options: [
-                    'blade' => 'Blade',
-                    'react' => 'React with Inertia',
-                    'vue' => 'Vue with Inertia',
-                    'api' => 'API only',
-                ]
-            ));
-        }
-
-        if (in_array($input->getOption('stack'), ['react', 'vue']) && (! $input->getOption('dark') || ! $input->getOption('ssr'))) {
-            collect(multiselect(
-                label: 'Would you like any optional features?',
-                options: [
-                    'dark' => 'Dark mode',
-                    'ssr' => 'Inertia SSR',
-                    'typescript' => 'TypeScript (experimental)',
-                ],
-                default: array_filter([
-                    $input->getOption('dark') ? 'dark' : null,
-                    $input->getOption('ssr') ? 'ssr' : null,
-                    $input->getOption('typescript') ? 'typescript' : null,
-                ]),
-            ))->each(fn ($option) => $input->setOption($option, true));
-        } elseif ($input->getOption('stack') === 'blade' && ! $input->getOption('dark')) {
-            $input->setOption('dark', confirm(
-                label: 'Would you like dark mode support?',
-                default: false,
-            ));
-        }
-    }
-
-    /**
-     * Determine the stack for Jetstream.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @return void
-     */
-    protected function promptForJetstreamOptions(InputInterface $input)
-    {
-        if (! $input->getOption('stack')) {
-            $input->setOption('stack', select(
-                label: 'Which Jetstream stack would you like to install?',
-                options: [
-                    'livewire' => 'Livewire',
-                    'inertia' => 'Vue with Inertia',
-                ]
-            ));
-        }
-
-        collect(multiselect(
-            label: 'Would you like any optional features?',
-            options: collect([
-                'api' => 'API support',
-                'dark' => 'Dark mode',
-                'verification' => 'Email verification',
-                'teams' => 'Team support',
-            ])->when(
-                $input->getOption('stack') === 'inertia',
-                fn ($options) => $options->put('ssr', 'Inertia SSR')
-            )->all(),
-            default: array_filter([
-                $input->getOption('api') ? 'api' : null,
-                $input->getOption('dark') ? 'dark' : null,
-                $input->getOption('teams') ? 'teams' : null,
-                $input->getOption('verification') ? 'verification' : null,
-                $input->getOption('stack') === 'inertia' && $input->getOption('ssr') ? 'ssr' : null,
-            ]),
-        ))->each(fn ($option) => $input->setOption($option, true));
-    }
-
-    protected function validateStackOption(InputInterface $input)
-    {
-        if ($input->getOption('breeze')) {
-            if (! in_array($input->getOption('stack'), $stacks = ['blade', 'react', 'vue', 'api'])) {
-                throw new \InvalidArgumentException("Invalid Breeze stack [{$input->getOption('stack')}]. Valid options are: ".implode(', ', $stacks).'.');
-            }
-
-            return;
-        }
-
-        if ($input->getOption('jet')) {
-            if (! in_array($input->getOption('stack'), $stacks = ['inertia', 'livewire'])) {
-                throw new \InvalidArgumentException("Invalid Jetstream stack [{$input->getOption('stack')}]. Valid options are: ".implode(', ', $stacks).'.');
-            }
-
-            return;
-        }
-    }
-
-    /**
-     * Install Pest into the application.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function installPest(string $directory, InputInterface $input, OutputInterface $output)
-    {
-        if ($this->removeComposerPackages(['phpunit/phpunit'], $output, true)
-            && $this->requireComposerPackages(['pestphp/pest:^2.0', 'pestphp/pest-plugin-laravel:^2.0'], $output, true)) {
-            $commands = array_filter([
-                $this->phpBinary().' ./vendor/bin/pest --init',
-            ]);
-
-            $this->runCommands($commands, $input, $output, workingPath: $directory, env: [
-                'PEST_NO_SUPPORT' => 'true',
-            ]);
-
-            $this->replaceFile(
-                'pest/Feature.php',
-                $directory.'/tests/Feature/ExampleTest.php',
-            );
-
-            $this->replaceFile(
-                'pest/Unit.php',
-                $directory.'/tests/Unit/ExampleTest.php',
-            );
-
-            $this->commitChanges('Install Pest', $directory, $input, $output);
-        }
-    }
-
-    /**
-     * Create a Git repository and commit the base Laravel skeleton.
-     *
-     * @param  string  $directory
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function createRepository(string $directory, InputInterface $input, OutputInterface $output)
-    {
-        $branch = $input->getOption('branch') ?: $this->defaultBranch();
-
-        $commands = [
-            'git init -q',
-            'git add .',
-            'git commit -q -m "Set up a fresh Laravel app"',
-            "git branch -M {$branch}",
-        ];
-
-        $this->runCommands($commands, $input, $output, workingPath: $directory);
-    }
-
-    /**
-     * Commit any changes in the current working directory.
-     *
-     * @param  string  $message
-     * @param  string  $directory
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function commitChanges(string $message, string $directory, InputInterface $input, OutputInterface $output)
-    {
-        if (! $input->getOption('git') && $input->getOption('github') === false) {
-            return;
-        }
-
-        $commands = [
-            'git add .',
-            "git commit -q -m \"$message\"",
-        ];
-
-        $this->runCommands($commands, $input, $output, workingPath: $directory);
-    }
-
-    /**
-     * Create a GitHub repository and push the git log to it.
-     *
-     * @param  string  $name
-     * @param  string  $directory
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    protected function pushToGitHub(string $name, string $directory, InputInterface $input, OutputInterface $output)
-    {
-        $process = new Process(['gh', 'auth', 'status']);
-        $process->run();
-
-        if (! $process->isSuccessful()) {
-            $output->writeln('  <bg=yellow;fg=black> WARN </> Make sure the "gh" CLI tool is installed and that you\'re authenticated to GitHub. Skipping...'.PHP_EOL);
-
-            return;
-        }
-
-        $name = $input->getOption('organization') ? $input->getOption('organization')."/$name" : $name;
-        $flags = $input->getOption('github') ?: '--private';
-
-        $commands = [
-            "gh repo create {$name} --source=. --push {$flags}",
-        ];
-
-        $this->runCommands($commands, $input, $output, workingPath: $directory, env: ['GIT_TERMINAL_PROMPT' => 0]);
     }
 
     /**
